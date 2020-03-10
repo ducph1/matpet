2eb4f367eb772652e167467a9c5274d92abea59d
vietduc122 <duc.do@j-job.com.vn>
2020-03-10 22:17:31 +0700

    Video15-add-and-list-menus

--- resources/views/menus/add.blade.php
+++ resources/views/menus/add.blade.php
@@ -22,14 +22,14 @@
                                 <input type="text"
                                        class="form-control"
                                        name="name"
-                                       placeholder="Nhập tên danh mục"
+                                       placeholder="Nhập tên menu"
                                 >
                             </div>
 
                             <div class="form-group">
                                 <label>Chọn menus cha</label>
                                 <select class="form-control" name="parent_id">
-                                    <option value="0">Chọn danh mục cha</option>
+                                    <option value="0">Chọn menu cha</option>
                                     {!! $optionSelect !!}
                                 </select>
                             </div>
