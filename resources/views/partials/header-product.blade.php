a4228efa3eea85542026189a34e3861b6d86592b
vietduc122 <duc.do@j-job.com.vn>
2020-03-28 22:43:16 +0700

    Video63-Format-Code-Header-Search

--- resources/views/partials/header-product.blade.php
+++ resources/views/partials/header-product.blade.php
@@ -44,7 +44,7 @@
         <!-- Messages Dropdown Menu -->
         <li class="nav-item">
             <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-                        class="fas fa-th-large"></i></a>
+                    class="fas fa-th-large"></i></a>
         </li>
     </ul>
 </nav>
