f5eea0b87368af8cdbe106fe539f4a7d4f758f74
vietduc122 <duc.do@j-job.com.vn>
2020-04-11 22:08:36 +0700

    Video54-Update-Form-Settings

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,12 +24,16 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Gate::define('category-list', function ($user) {
-           return $user->checkPermissionAccess(config('permissions.access.list-category'));
-        });
+        Gate::define('category-list', 'App\Policies\CategoryPolicy@view');
+        Gate::define('category-add', 'App\Policies\CategoryPolicy@create');
+        Gate::define('category-edit', 'App\Policies\CategoryPolicy@update');
+        Gate::define('category-delete', 'App\Policies\CategoryPolicy@delete');
+
 
         Gate::define('menu-list', function ($user) {
             return $user->checkPermissionAccess(config('permissions.access.list-menu'));
         });
+
+
     }
 }
