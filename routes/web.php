3d478a7e8cd3a909fa064f13885cf4cff6ca14f4
vietduc122 <duc.do@j-job.com.vn>
2020-04-11 14:33:07 +0700

    Video89-Permission-Add-Phan-4

--- routes/web.php
+++ routes/web.php
@@ -252,7 +252,12 @@ Route::prefix('admin')->group(function () {
     Route::prefix('permissions')->group(function () {
         Route::get('/create', [
             'as' => 'permissions.create',
-            'uses' => 'AdminRoleController@createPermissions'
+            'uses' => 'AdminPermissionController@createPermissions'
+        ]);
+
+        Route::post('/store', [
+            'as' => 'permissions.store',
+            'uses' => 'AdminPermissionController@store'
         ]);
 
 
